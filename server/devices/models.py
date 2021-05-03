4a7b240f692f51c1e89b98a686a4a59b69807b44
vahidzee <vahid98zee@gmail.com>
2021-05-03 22:11:57 +0430

    add Face & Log models
    * Faces hold information of each users known faces
    * Logs hold intermittent information each device sends about faces entering or leaving the property

--- server/devices/models.py
+++ server/devices/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
+from django.utils.html import format_html
 
 
 # Create your models here.
@@ -8,4 +10,48 @@ class Device(models.Model):
     name = models.CharField(help_text='Custom name for device', max_length=64, blank=True)
     user = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=True, null=True)
 
+    def __str__(self):
+        return f'Device(id="{self.id}")'
+
     # todo: add count of people inside
+
+
+def picture_path(instance, filename):
+    base_dir = f'faces/{instance.user}' if isinstance(
+        instance, Face) else f'log/{instance.device}/{instance.face}/{instance}'
+    filename = f"{instance.face_id}-{filename.strip().replace(' ', '_')}" if isinstance(
+        instance, Face) else f'{instance.time}.{filename.split(".")[-1]}'
+    return f'{base_dir}/{filename}'
+
+
+class Face(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=False, null=False)
+    face_id = models.UUIDField(default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=80, blank=True, help_text='Custom name for surveilled face')
+    image = models.ImageField(upload_to=picture_path, blank=False)
+    embedding = models.TextField(blank=True, null=True)
+
+    class Meta:
+        unique_together = ('user', 'face_id',)
+
+    def picture(self):
+        res = f'<img src="{self.image.url}" width="50vw">'
+        return format_html(res)
+
+    def __str__(self):
+        return f'Face(name="{self.name}")'
+
+
+class Log(models.Model):
+    face = models.ForeignKey(to=Face, blank=False, null=False, on_delete=models.CASCADE)
+    device = models.ForeignKey(to=Device, blank=False, null=False, on_delete=models.CASCADE)
+    time = models.DateTimeField(auto_now_add=True)
+    picture = models.ImageField(upload_to=picture_path)
+    # kind
+    ENTRY = 'E'
+    EXIT = 'L'
+    kind_choices = [
+        (ENTRY, 'Entered'),
+        (EXIT, 'Left')
+    ]
+    kind = models.CharField(max_length=1, null=False, blank=False, choices=kind_choices)
--- server/surveillance/settings.py
+++ server/surveillance/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'devices'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'surveillance.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +125,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
